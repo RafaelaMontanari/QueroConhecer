3d73a0363c6d25d5de6550d06c4850003d60d19f
RafaelaMontanari <rafaela_silva_cunha@hotmail.com>
2020-07-05 09:50:23 -0300

    Add app

---
+++ QueroConhecer/Controllers/PlaceFinderViewController.swift
@@ -0,0 +1,19 @@
+//
+//  PlaceFinderViewController.swift
+//  QueroConhecer
+//
+//  Created by Rafaela on 04/07/20.
+//  Copyright © 2020 Rafaela. All rights reserved.
+//
+
+import UIKit
+
+class PlaceFinderViewController: UIViewController {
+
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+}
---
+++ QueroConhecer/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  QueroConhecer
+//
+//  Created by Rafaela on 04/07/20.
+//  Copyright © 2020 Rafaela. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
